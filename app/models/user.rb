5427166ef8c5099b09c5a2b54a42eccf11ca737d
Grace Shih <vocalise@gmail.com>
2018-09-10 01:57:43 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
+require_relative '../models/concerns/slugifiable'
+
 class User < ActiveRecord::Base
+  extend Slugifiable::ClassMethods
+  include Slugifiable::InstanceMethods
+  
+  has_secure_password
   has_many :tweets
+  
+  
 end
--- db/migrate/20180909023528_create_usertweets.rb
+++
@@ -1,8 +0,0 @@
-class CreateUsertweets < ActiveRecord::Migration
-  def change
-    create_table :user_tweets do |t|
-      t.integer :user_id
-      t.integer :tweet_id
-    end
-  end
-end
