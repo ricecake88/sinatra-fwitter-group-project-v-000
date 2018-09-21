7a7fc129104456e47182550583b79368c0915d96
Grace Shih <vocalise@gmail.com>
2018-09-21 07:21:18 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -52,7 +52,10 @@ class UsersController < ApplicationController
 
   get '/users/:slug' do
     @user = User.find_by_slug(params[:slug])
-    binding.pry
-    erb :'/tweets/show_tweet'
+    if logged_in?
+      erb :'/tweets/show_tweet'
+    else
+      redirect to '/login'
+    end
   end
 end
