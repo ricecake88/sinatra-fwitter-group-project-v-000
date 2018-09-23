4e56a03a6ef2cb2d3f7a6f3aa3cd22ad0348f723
Grace Shih <vocalise@gmail.com>
2018-09-23 04:33:30 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -14,4 +14,69 @@ class TweetsController < ApplicationController
       redirect to '/login'
     end
   end
+  
+  post '/tweets/new' do
+    content = params[:content]
+    if logged_in?
+      if content != ""
+        tweet = Tweet.new(user_id:session[:user_id], content:params[:content])
+        current_user.tweets << tweet
+        redirect to '/tweets'
+      end
+    else
+      redirect to '/login'
+    end
+  end
+  
+  get '/tweets/:id' do
+    if logged_in?
+      @tweet = Tweet.find(params[:id])
+      erb :'/tweets/show_tweet'
+    else
+      redirect to '/login'
+    end
+  end
+  
+  get '/tweets/:id/edit' do 
+    if logged_in?
+      @tweet = Tweet.find_by_id(params[:id])
+      erb :'/tweets/edit_tweet'
+    else
+      redirect to '/login'
+    end
+  end
+  
+  get '/tweets/:id/delete' do
+    if logged_in?
+      @tweet = Tweet.find_by_id(params[:id])
+      erb :'/tweets/edit_tweet'
+    else
+      redirect to '/login'
+    end
+  end
+  
+  patch '/tweets/:id/edit' do
+    if logged_in?
+      if params[:content] != ""
+        @tweet = Tweet.find_by(params[:id])
+        @tweet.content = params[:content]
+        @tweet.save
+        redirect to "/tweets/#{@tweet.id}"
+      end
+    else
+      redirect to '/login'
+    end
+  end
+  
+  delete '/tweets/:id/delete' do
+    if logged_in?
+      @tweet = Tweet.find(params[:id])
+      if current_user.id == @tweet.user_id
+        @tweet.delete
+      end
+      redirect to '/tweets'
+    else
+      redirect to '/login'
+    end
+  end
 end
