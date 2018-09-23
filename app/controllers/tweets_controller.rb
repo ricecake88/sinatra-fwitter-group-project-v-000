86163be1c6050d196bcdd57a33386595a6f2cdca
Grace Shih <vocalise@gmail.com>
2018-09-23 04:10:14 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -45,6 +45,14 @@ class TweetsController < ApplicationController
     end
   end
   
+  patch '/tweets/:id/edit' do
+    if logged_in?
+      @tweet = Tweet.find_by(params[:id])
+    else
+      redirect to '/login'
+    end
+  end
+  
   delete '/posts/:id/delete' do
     if logged_in?
       @tweet = Tweet.find(params[:id])
